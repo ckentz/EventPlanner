097e814784a204a402a726e62c2e48b159e203f9
ckentz <ckentz@holyfamily.edu>
2025-03-22 20:27:45 +0000

    created event form

--- events/views.py
+++ events/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import EventForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def create_event(request):
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            event = form.save(commit=False)
+            event.organizer = request.user
+            event.save()
+            return redirect('event_list')  # we'll make this view next
+    else:
+        form = EventForm()
+    return render(request, 'events/create_event.html', {'form': form})
