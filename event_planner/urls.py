f6bbe3ca37e4a82c989fd1fc59cbd2fed4b63ccd
ckentz <ckentz@holyfamily.edu>
2025-03-24 00:28:36 +0000

    Added edit and delete forms

--- event_planner/urls.py
+++ event_planner/urls.py
@@ -5,5 +5,6 @@ from users.views import home
 urlpatterns = [
     path('', home, name="home"),
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),  # Add this line
+    path('users/', include('users.urls')),
+    path('events/', include('events.urls')),
 ]
--- events/views.py
+++ events/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import EventForm
+from .models import Event
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+
 
 @login_required
 def create_event(request):
@@ -10,7 +13,32 @@ def create_event(request):
             event = form.save(commit=False)
             event.organizer = request.user
             event.save()
-            return redirect('event_list')  # we'll make this view next
+            return redirect('event_list')
     else:
         form = EventForm()
     return render(request, 'events/create_event.html', {'form': form})
+
+@login_required
+def event_list(request):
+    events = Event.objects.filter(organizer=request.user)
+    return render(request, 'events/event_list.html', {'events': events})
+
+@login_required
+def edit_event(request, event_id):
+    event = get_object_or_404(Event, id=event_id, organizer=request.user)
+    if request.method == 'POST':
+        form = EventForm(request.POST, instance=event)
+        if form.is_valid():
+            form.save()
+            return redirect('event_list')
+    else:
+        form = EventForm(instance=event)
+    return render(request, 'events/edit_event.html', {'form': form, 'event': event})
+
+@login_required
+def delete_event(request, event_id):
+    event = get_object_or_404(Event, id=event_id, organizer=request.user)
+    if request.method == 'POST':
+        event.delete()
+        return redirect('event_list')
+    return render(request, 'events/delete_event.html', {'event': event})
