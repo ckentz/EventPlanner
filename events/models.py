b3c84da6467541cf39299eac71905db630fd6d6f
ckentz <ckentz@holyfamily.edu>
2025-04-08 17:43:22 +0000

    added rsvp model

--- events/models.py
+++ events/models.py
@@ -8,5 +8,16 @@ class Event(models.Model):
     location = models.CharField(max_length=200)
     organizer = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    from django.db import models
+from django.contrib.auth.models import User
+
+class RSVP(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey('Event', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'event')  # to prevent duplicates
+
     def __str__(self):
-        return self.title
+        return f"{self.user.username} RSVP'd to {self.event.title}"
