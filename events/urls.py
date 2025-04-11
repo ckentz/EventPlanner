6b03eabad02389e055e0eeae75ad55d04c91d17b
ckentz <ckentz@holyfamily.edu>
2025-04-11 22:39:20 +0000

    Added date and time sorting

--- events/urls.py
+++ events/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', views.event_list, name='event_list'),
     path('edit/<int:event_id>/', views.edit_event, name='edit_event'),
     path('delete/<int:event_id>/', views.delete_event, name='delete_event'),
+    path('detail/<int:event_id>/', views.event_detail, name='event_detail'),
+    path('browse/', views.browse_events, name='browse_events'),
+
 ]
--- events/views.py
+++ events/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
-from .forms import EventForm
-from .models import Event
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404
+from django.contrib import messages
+from .models import Event, RSVP
+from .forms import EventForm
 
+# Event CRUD Views
 
 @login_required
 def create_event(request):
@@ -18,10 +19,13 @@ def create_event(request):
         form = EventForm()
     return render(request, 'events/create_event.html', {'form': form})
 
+
 @login_required
 def event_list(request):
-    events = Event.objects.filter(organizer=request.user)
+    events = Event.objects.filter(organizer=request.user).order_by('date')
     return render(request, 'events/event_list.html', {'events': events})
+
+
 
 @login_required
 def edit_event(request, event_id):
@@ -35,6 +39,7 @@ def edit_event(request, event_id):
         form = EventForm(instance=event)
     return render(request, 'events/edit_event.html', {'form': form, 'event': event})
 
+
 @login_required
 def delete_event(request, event_id):
     event = get_object_or_404(Event, id=event_id, organizer=request.user)
@@ -42,3 +47,34 @@ def delete_event(request, event_id):
         event.delete()
         return redirect('event_list')
     return render(request, 'events/delete_event.html', {'event': event})
+
+
+# RSVP + Event Detail
+
+@login_required
+def event_detail(request, event_id):
+    event = get_object_or_404(Event, id=event_id)
+    is_attending = RSVP.objects.filter(user=request.user, event=event).exists()
+    attendees = RSVP.objects.filter(event=event)
+
+    if request.method == 'POST':
+        if is_attending:
+            RSVP.objects.filter(user=request.user, event=event).delete()
+            messages.success(request, "You have canceled your RSVP.")
+        else:
+            RSVP.objects.create(user=request.user, event=event)
+            messages.success(request, "You have RSVP'd to this event.")
+        return redirect('event_detail', event_id=event.id)
+
+    return render(request, 'events/event_detail.html', {
+        'event': event,
+        'attendees': attendees,
+        'is_attending': is_attending,
+    })
+
+# Public Browsing
+
+@login_required
+def browse_events(request):
+    events = Event.objects.all().order_by('date')
+    return render(request, 'events/browse_events.html', {'events': events})
