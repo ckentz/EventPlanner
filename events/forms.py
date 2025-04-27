1c0a6136039b0b918d2c9d6158357e6eda85681d
ckentz <ckentz@holyfamily.edu>
2025-04-27 22:58:04 +0000

    styles, login and register

--- events/forms.py
+++ events/forms.py
@@ -1,7 +1,22 @@
 from django import forms
 from .models import Event
+from datetime import datetime
 
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = ['title', 'description', 'date', 'location']
+        fields = ['title', 'description', 'location', 'date']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'location': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(EventForm, self).__init__(*args, **kwargs)
+        now = datetime.now().strftime('%Y-%m-%dT%H:%M')  # format for datetime-local
+        self.fields['date'].widget = forms.DateTimeInput(attrs={
+            'type': 'datetime-local',
+            'class': 'form-control',
+            'min': now,
+        })
